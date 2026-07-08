b7d75385d0a886d3e99c89311843c81cba9ceb26
Ana Carolina Schneider <anacschneider558@gmail.com>
2026-07-08 15:51:47 -0300

    Projeto inicial Spring Boot

---
+++ src/main/java/com/ana/pesquisa_online/PesquisaOnlineApplication.java
@@ -0,0 +1,17 @@
+package com.ana.pesquisa_online;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PesquisaOnlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PesquisaOnlineApplication.class, args);
+	} {
+        System.out.println("Projeto");
+    }
+
+
+
+}
---
+++ src/test/java/com/ana/pesquisa_online/PesquisaOnlineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ana.pesquisa_online;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PesquisaOnlineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
